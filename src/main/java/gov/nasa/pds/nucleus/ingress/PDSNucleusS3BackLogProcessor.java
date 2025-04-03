774c6a70c054e9b908ac8408804362fa928a2b99
Ramesh Maddegoda <94033485+ramesh-maddegoda@users.noreply.github.com>
2025-04-03 12:15:34 -0700

    ADD Initial code of s3 backlog processor.
    Refer to the task https://github.com/NASA-PDS/nucleus/issues/142

--- src/main/java/gov/nasa/pds/nucleus/ingress/PDSNucleusS3BackLogProcessor.java
+++ src/main/java/gov/nasa/pds/nucleus/ingress/PDSNucleusS3BackLogProcessor.java
@@ -1,3 +1,33 @@
+// Copyright © 2025 , California Institute of Technology ("Caltech").
+// U.S. Government sponsorship acknowledged.
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// • Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+// • Redistributions must reproduce the above copyright notice, this list of
+//   conditions and the following disclaimer in the documentation and/or other
+//   materials provided with the distribution.
+// • Neither the name of Caltech nor its operating division, the Jet Propulsion
+//   Laboratory, nor the names of its contributors may be used to endorse or
+//   promote products derived from this software without specific prior written
+//   permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
 package gov.nasa.pds.nucleus.ingress;
 
 import software.amazon.awssdk.regions.Region;
@@ -7,8 +37,18 @@ import software.amazon.awssdk.services.s3.model.ListObjectsV2Response;
 import software.amazon.awssdk.services.s3.model.S3Object;
 import software.amazon.awssdk.services.sqs.SqsClient;
 import software.amazon.awssdk.services.sqs.model.SendMessageRequest;
+
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/**
+ * This class is used to process files that have already been transferred to an S3 bucket,
+ * and will not have an S3 trigger to start the baseline Nucleus DAG.
+ * More details are available at <a href="https://github.com/NASA-PDS/nucleus/issues/141">...</a>
+ *
+ * @author The Planetary Data System
+ * @since 2025
+ */
 public class PDSNucleusS3BackLogProcessor {
 
     private static final Logger logger = Logger.getLogger(PDSNucleusS3BackLogProcessor.class.getName());
@@ -17,20 +57,28 @@ public class PDSNucleusS3BackLogProcessor {
 
         String s3BucketName = System.getenv("S3_BUCKET_NAME");
         String s3BucketPrefix =  System.getenv("S3_BUCKET_PREFIX");
+        String sqsQueueURL =  System.getenv("SQS_QUEUE_URL");
         Region awsRegion = Region.of(System.getenv("AWS_REGION"));
 
-
+        logger.setLevel(Level.FINE);
         logger.info("s3BucketName = " + s3BucketName);
         logger.info("s3BucketPrefix = " + s3BucketPrefix);
         logger.info("Region = " + awsRegion.toString());
         logger.info("Processing S3 Backlog at: " + s3BucketPrefix);
 
         PDSNucleusS3BackLogProcessor pdsNucleusS3BackLogProcessor = new PDSNucleusS3BackLogProcessor();
-        pdsNucleusS3BackLogProcessor.listObjectsInBucket(s3BucketName, s3BucketPrefix, awsRegion);
+        pdsNucleusS3BackLogProcessor.listObjectsInBucket(s3BucketName, s3BucketPrefix, sqsQueueURL, awsRegion);
 
     }
 
-    void listObjectsInBucket(String s3BucketName, String s3BucketPrefix, Region awsRegion) {
+    /**
+     * Lists objects in a given S3 bucket and sends messages to an SQS queue.
+     * @param s3BucketName Name of the S3 bucket
+     * @param s3BucketPrefix The prefix (path) in S3 bucket to start listing objects from
+     * @param sqsQueueURL URL of the SQS Queue
+     * @param awsRegion AWS Region
+     */
+    void listObjectsInBucket(String s3BucketName, String s3BucketPrefix, String sqsQueueURL, Region awsRegion) {
         S3Client s3Client = S3Client.builder()
                 .region(awsRegion)
                 .build();
@@ -52,17 +100,13 @@ public class PDSNucleusS3BackLogProcessor {
             ListObjectsV2Response listObjResponse = s3Client.listObjectsV2(listObjectsReqManual);
 
             pageNumber++;
-            System.out.println("Page size of page " + pageNumber + ": " + listObjResponse.contents().size());
+
+            logger.info("Page size of S3 object list: " + pageNumber + ": " + listObjResponse.contents().size());
 
             for (S3Object s3Object : listObjResponse.contents()) {
-                System.out.println(s3Object.key());
-
                 String event = "{\"backlog\":\"true\",\"s3_bucket\":\""+ s3BucketName + "\", \"s3_key\":\"" + s3Object.key() + "\"}";
-
-                System.out.println("Sending event: " + event);
-
-                sendMessage(sqsClient,
-                        "https://sqs.us-west-2.amazonaws.com/441083951559/pds-nucleus-files-to-save-in-database-PDS_SBN", event);
+                logger.info("Sending event: " + event);
+                sendMessage(sqsClient, sqsQueueURL, event);
             }
 
             if (listObjResponse.nextContinuationToken() == null) {
@@ -74,11 +118,16 @@ public class PDSNucleusS3BackLogProcessor {
                     .build();
         }
 
-
-
         s3Client.close();
     }
 
+    /**
+     * Sends a message to a SQS queue.
+     *
+     * @param sqsClient SQS Client
+     * @param queueURL SQS Queue URL
+     * @param message Message
+     */
     void sendMessage(SqsClient sqsClient, String queueURL, String message) {
         try {
 
@@ -91,7 +140,7 @@ public class PDSNucleusS3BackLogProcessor {
             sqsClient.sendMessage(sendMsgRequest);
 
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.severe(e.toString());
             System.exit(1);
         }
     }
